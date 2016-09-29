aa55ce61d54f9aed659590c265fc265c81989c1b
Nils Wloka <nils.wloka@codecentric.de>
2016-09-29 13:23:15 +0200

    Add default assets to template

--- generator-cclz-react-native/app/index.js
+++ generator-cclz-react-native/app/index.js
@@ -46,7 +46,7 @@ module.exports = generators.Base.extend({
             this.name + '/.editorconfig'
         );
         this.copy(
-            '.gitignore',
+            'gitignore',
             this.name + '/.gitignore'
         );
         this.copy(
@@ -57,8 +57,15 @@ module.exports = generators.Base.extend({
             'merge-base-ff.sh',
             this.name + '/merge-base-ff.sh'
         );
+        this.copy(
+            'README.md',
+            this.name + '/README.md'
+        );
         this.directory('src', this.name + '/src');
         this.directory('test', this.name + '/test');
+        this.directory('android', this.name + '/android');
+        this.directory('Images.xcassets', this.name + '/ios/' + this.name + '/Images.xcassets');
+        this.directory('default-assets', this.name + '/default-assets');
     }
 
 });
---
+++ generator-cclz-react-native/app/templates/README.md
@@ -0,0 +1,3 @@
+# Next steps
+Please follow the instructions given on our [documentation site](https://codecentric-labs-zero.github.io/pages/react-native.html) to prepare
+your newly created app for deployment.
